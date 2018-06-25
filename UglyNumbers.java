febee3a1af4c82a1579a087b904462f0a7374ea8
Lz10000 <lzheng@gmail.com>
2018-06-25 15:31:24 -0700

    first commit

---
+++ UglyNumbers.java
@@ -0,0 +1,9 @@
+/*
+
+Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1,2,3,4,5,6,8,10,12,15 shows the first 11 ugly numbers. By convention 1 is included. Given a number n, the task is find the nth Ugly number. 
+
+*/
+
+int getUglyNum(int n){
+                        
+
